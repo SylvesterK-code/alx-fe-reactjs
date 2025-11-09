4759aeeca393761ee841d19390e18d779da19b18
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-11-09 19:20:59 +0000

    3. Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Services from "./Components/Services";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 // function App() {
 //   return (
