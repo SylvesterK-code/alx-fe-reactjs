5f60e4d4028034827c4d94461d1ec8786fe07595
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-11-27 10:14:27 +0000

    Responsive Design Implementation Using Tailwind CSS - added sm:p-4, md:max-w-sm, md:text-xl, md:w-36, md:h-36

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -97,8 +97,8 @@ function UserProfile() {
     <div
       className="
         bg-white 
-        p-4 sm:p-4 sm:p-6 md:p-8 
-        max-w-xs sm:max-w-sm md:max-w-sm 
+        p-4 sm:p-4 md:p-8 
+        max-w-xs sm:max-w-sm md:max-w-sm
         mx-auto my-10 
         rounded-xl shadow-lg text-center
       "
@@ -107,28 +107,24 @@ function UserProfile() {
       <div className="flex justify-center mb-4">
         <svg
           viewBox="0 0 512 512"
-          className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 text-gray-800"
+          className="w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 text-gray-800"
           fill="currentColor"
         >
-          <path
-            d="M458.159,404.216c-18.93-33.65-49.934-71.764-100.409-93.431c-28.868,20.196-63.938,32.087-101.745,32.087
+          <path d="M458.159,404.216c-18.93-33.65-49.934-71.764-100.409-93.431c-28.868,20.196-63.938,32.087-101.745,32.087
           c-37.828,0-72.898-11.89-101.767-32.087c-50.474,21.667-81.479,59.782-100.398,93.431C28.731,448.848,48.417,512,91.842,512
-          c43.426,0,164.164,0,164.164,0s120.726,0,164.153,0C463.583,512,483.269,448.848,458.159,404.216z"
-          />
-          <path
-            d="M256.005,300.641c74.144,0,134.231-60.108,134.231-134.242v-32.158C390.236,60.108,330.149,0,256.005,0
-          c-74.155,0-134.252,60.108-134.252,134.242V166.4C121.753,240.533,181.851,300.641,256.005,300.641z"
-          />
+          c43.426,0,164.164,0,164.164,0s120.726,0,164.153,0C463.583,512,483.269,448.848,458.159,404.216z" />
+          <path d="M256.005,300.641c74.144,0,134.231-60.108,134.231-134.242v-32.158C390.236,60.108,330.149,0,256.005,0
+          c-74.155,0-134.252,60.108-134.252,134.242V166.4C121.753,240.533,181.851,300.641,256.005,300.641z" />
         </svg>
       </div>
 
       {/* Name */}
-      <h1 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-900">
+      <h1 className="text-lg sm:text-xl md:text-xl font-semibold text-gray-900">
         Sylvester K. Nyadzinor
       </h1>
 
       {/* Role */}
-      <p className="text-sm sm:text-base md:text-xl text-gray-500 mb-4">
+      <p className="text-sm sm:text-base md:text-lg text-gray-500 mb-4">
         Software Engineer
       </p>
 
@@ -171,3 +167,4 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
