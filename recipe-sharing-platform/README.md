f36677f151b5f71dbb6f33e1359693336dc3b842
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-12-03 05:24:02 +0000

    Setup a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/README.md
@@ -0,0 +1,8 @@
+npm create vite@latest recipe-sharing-platforact
+npm install -D tailwindcss postcss autoprefixer
+npm install tailwindcss @tailwindcss/vite
+npx tailwindcss init -p     not work in tailwind v4
+ npx tailwindcss init       not work in tailwind v4
+
+npm install tailwindcss
+
---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold text-green-600">Tailwind 4 works!</h1>
+  );
+}
+
+export default App
