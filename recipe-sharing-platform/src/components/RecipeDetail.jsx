ac9a161734a387b754808f0a18742bfd6305fdcb
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-12-03 10:44:25 +0000

    separated import { useParams, Link} from 'react-router-dom' into individual import components

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import {Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import Button from "./ui/Button";
 
