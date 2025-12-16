b7b0ca2d02289e13a4251a6b3a4b44598bfb0788
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-12-16 05:53:59 +0000

    Advanced Routing in React with React Router - updated App.jsx with BrowserRouter

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,32 +1,81 @@
+// // src/App.jsx
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Profile from "./components/Profile";
+// import BlogPost from "./pages/BlogPost";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/login" element={<Login />} />
+
+//       {/* Protected Route */}
+//       <Route
+//         path="/profile/*"
+//         element={
+//           <ProtectedRoute>
+//             <Profile />
+//           </ProtectedRoute>
+//         }
+//       />
+
+//       {/* Dynamic Route */}
+//       <Route path="/posts/:postId" element={<BlogPost />} />
+
+//       <Route path="*" element={<Navigate to="/" />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
 // src/App.jsx
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
+import "./index.css"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
 
-      {/* Protected Route */}
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+        {/* Protected Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Dynamic Route */}
-      <Route path="/posts/:postId" element={<BlogPost />} />
+        {/* Dynamic Route */}
+        <Route path="/posts/:postId" element={<BlogPost />} />
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
