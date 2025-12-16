467321ec11e2944d48a0504ac24154f70f87c617
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-12-16 06:03:36 +0000

    Advanced Routing in React with React Router - updated App.jsx with /blog/:id

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -48,10 +48,10 @@ import {
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Profile from "./components/Profile";
+import Profile from "./pages/Profile"; // make sure this is in pages/
 import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
-import "./index.css"
+import "./index.css";
 
 function App() {
   return (
@@ -59,7 +59,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
         {/* Protected Route */}
         <Route
           path="/profile/*"
@@ -70,9 +69,11 @@ function App() {
           }
         />
 
-        {/* Dynamic Route */}
+        {/* handle both /blog/:id and /posts/:postId --- works the same*/}
+        {/* Dynamic Routes */}
         <Route path="/posts/:postId" element={<BlogPost />} />
-
+        <Route path="/blog/:id" element={<BlogPost />} />
+        {/* Fallback Route */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
--- react-router-advanced/src/pages/BlogPost.jsx
+++ react-router-advanced/src/pages/BlogPost.jsx
@@ -1,8 +1,29 @@
+// import { useParams } from "react-router-dom";
+
+// const BlogPost = () => {
+//   const { postId } = useParams();
+
+//   return (
+//     <div>
+//       <h2>Blog Post</h2>
+//       <p>Post ID: {postId}</p>
+//     </div>
+//   );
+// };
+
+// export default BlogPost;
+
+
+
+
+
+
+
 import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
-  const { postId } = useParams();
-
+  const params = useParams();
+  const postId = params.postId || params.id; // handle both /blog/:id and /posts/:postId
   return (
     <div>
       <h2>Blog Post</h2>
