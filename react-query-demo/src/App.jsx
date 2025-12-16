b4f4faab616a11555567ad990decc09af431dd11
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-12-16 04:17:57 +0000

    Advanced Data Handling with React Query - updated app.jsx to include QueryClient, QueryClientProvider, queryClient, client={queryClient}

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,15 +1,20 @@
-
-import './index.css'
-
+import "./index.css";
 import React from "react";
 import PostsComponent from "./components/PostsComponent";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen bg-gray-50">
+        <h1 className="text-center text-3xl font-bold py-6">
+          React Query Demo
+        </h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,10 +1,26 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-import './index.css'
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+
+
+// src/main.jsx
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
